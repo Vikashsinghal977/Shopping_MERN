4be4ed4df359216101cb163655f630d876e2f36a
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-18 17:21:09 +0530

    Handle the casting, promise rejection and uncaught errors

--- Backend/server.js
+++ Backend/server.js
@@ -4,14 +4,29 @@ const dotenv = require("dotenv");
 
 const connectDatabase = require("./databases/database");
 
+// Handling Uncaught Exception
+process.on("uncaughtException",(err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Handling Uncaught Exception`);
+
+    process.exit(1);
+});
 
 // config
 dotenv.config({path:"Backend/config/config.env"});
 
 connectDatabase();
 
-
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`server is working on http://localhost:${process.env.PORT}`)
-}) 
- 
+})
+
+// unhandled promiss Rejction
+process.on("unhandledRejection", (err) => {
+    console.log(`Error ${err.message}`);
+    console.log(`Shutting down the server due to uhandled promise Rejection`);
+
+    server.close(()=>{
+        process.exit(1);
+    });
+})
