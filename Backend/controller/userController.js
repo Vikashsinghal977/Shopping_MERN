06ffc3f79afdf0e26e3c400e5486500dc044b391
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-23 17:02:25 +0530

    Forgot passowrd Emailer succefully

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -2,12 +2,11 @@ const ErrorHander = require("../utils/errorhandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
-
-
+const sendEmail = require("../utils/sendEmail")
 // Register a user
 
-exports.registerUser = catchAsyncError( async(req, res, next) => {
-    
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+
     const {
         name,
         email,
@@ -20,9 +19,9 @@ exports.registerUser = catchAsyncError( async(req, res, next) => {
         name,
         email,
         password,
-        avatar:{
-            publice_id:"this is a sample id",
-            url:"ProfilepicUrl"
+        avatar: {
+            publice_id: "this is a sample id",
+            url: "ProfilepicUrl"
         }
     });
 
@@ -30,30 +29,30 @@ exports.registerUser = catchAsyncError( async(req, res, next) => {
 });
 
 //Login user
-exports.loginUser = catchAsyncError(async (req, res, next)=>{
+exports.loginUser = catchAsyncError(async (req, res, next) => {
 
     const {
         email,
         password,
-    }= req.body;
+    } = req.body;
 
     // checking if your has given password and email both
 
-    if (!email  || !password){
-        return next(new ErrorHander("Please Enter email and passsowrd",400));
+    if (!email || !password) {
+        return next(new ErrorHander("Please Enter email and passsowrd", 400));
     }
 
-    const user = await User.findOne({email}).select("+password");
+    const user = await User.findOne({ email }).select("+password");
 
-    if (!user){
-        return next(new ErrorHander("Invelid email and password",401));
+    if (!user) {
+        return next(new ErrorHander("Invelid email and password", 401));
     }
 
     const isPasswordMatched = await user.comparePassword(password);
 
 
-    if (!isPasswordMatched){
-        return next(new ErrorHander("Invelid email and password",401));
+    if (!isPasswordMatched) {
+        return next(new ErrorHander("Invelid email and password", 401));
     }
 
     sendToken(user, 200, res);
@@ -70,8 +69,51 @@ exports.logout = catchAsyncError(async (req, res, next) => {
     })
 
     res.status(200).json({
-        success:true,
-        message:"Logged Out" 
+        success: true,
+        message: "Logged Out"
     })
 
+})
+
+
+// Forgot Password
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+
+    if (!user) {
+ 
+        return next(new ErrorHander("User not found,404"));
+    }
+
+    //Get ResetPassword token
+
+    const resetToken = user.getRestPasswordToken();
+
+
+    await user.save({ validateBeforeSave: false });
+
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+
+    const message = `your password reset token is :- \n\n ${resetPasswordUrl} \n\n if you not requested this email than, please ignore it`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `Ecommerce password Recovery`,
+            message,
+        })
+        res.status(200).json({
+            success: true,
+            message: `Email send to ${user.email} Successfully`
+        })
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined
+        await user.save({ validateBeforeSave: false });
+        return next(new ErrorHander(error.message, 500))
+    }
 })
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs/dist/bcrypt");
+const crypto = require("crypto")
 const userSchema = new mongoose.Schema({
 
     name:{
@@ -36,7 +37,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         default:"user",
     },
-    resetPassword:{
+    resetPasswordToken:{
         type:String,
     },
     resetPasswordExpire:{
@@ -61,10 +62,29 @@ userSchema.methods.getJWTToken = function () {
 }
 
 //compare password
-
 userSchema.methods.comparePassword = async function(enterpassword){
     
     return await bcrypt.compare(enterpassword,this.password)
 }
+
+
+//Generating Password Reset token
+userSchema.methods.getRestPasswordToken = function (){
+
+    // Generating Token
+    const resetToken = crypto.randomBytes(20).toString('hex')
+
+    // Hashing and add to userSchema
+    this.resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    this.resetPasswordExpire = Date.now() + 15 *60*1000;
+
+    return resetToken;
+}
+
+
 
 module.exports = mongoose.model("User",userSchema)
