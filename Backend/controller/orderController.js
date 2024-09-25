5e7734c0c7b5272f6fa983e5c5d06e5a988defac
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-25 14:48:56 +0530

    Create Get all orders  in Admin Apis

--- Backend/controller/orderController.js
+++ Backend/controller/orderController.js
@@ -3,8 +3,8 @@ const ErrorHander = require("../utils/errorhandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
 const Product = require("../models/productmodels");
 
+// Create New Order 
 exports.newOrder = catchAsyncError(async(req, res, next) => {
-    console.log("Start")
 
     const {
         shypingInfo,
@@ -32,4 +32,62 @@ exports.newOrder = catchAsyncError(async(req, res, next) => {
         success:true,
         order,
     })
+})
+
+// Get single order
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    console.log("strat get single order")
+    
+    const order = await Order.findById(req.params.id).populate("user","name email")
+
+    if (!order){
+        return next(new ErrorHander("Order not found", 404))
+    }
+
+    res.status(200).json({
+        success:true,
+        order,
+    })
+
+})
+
+
+// Get Logged in user order
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    console.log("Start myOrder")
+    
+    const orders = await Order.find({user:req.user._id});
+    console.log("orders",orders)
+    if (!orders){
+        return next(new ErrorHander("Order not found", 404))
+    }
+
+    res.status(200).json({
+        success:true,
+        orders,
+    })
+
+})
+
+
+// Get All order -> Admin
+exports.getAllOrder = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.find();
+
+    if (!orders){
+        return next(new ErrorHander("Order not found", 404))
+    }
+
+    let totalAmount = 0;
+
+    orders.forEach((order) => {
+        totalAmount += order.totalPrice;
+    });
+
+    res.status(200).json({
+        success:true,
+        orders,
+        totalAmount,
+    })
+
 })
--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { newOrder } = require("../controller/orderController");
+const { newOrder, getSingleOrder, myOrders, getAllOrder } = require("../controller/orderController");
 const { isAuthenticateUser, authorizRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticateUser,newOrder);
+router.route("/order/me").get(isAuthenticateUser,myOrders)
+router.route("/admin/orders").get(isAuthenticateUser, authorizRoles("admin"), getAllOrder)
+
+router.route("/order/:id").get(isAuthenticateUser,getSingleOrder)
+
 
 module.exports = router
