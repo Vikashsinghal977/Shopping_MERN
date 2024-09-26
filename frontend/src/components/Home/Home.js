e8cb45747e113ea59ceb4b10ae7f5b1bafd3e0bc
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-26 17:05:56 +0530

    Make a Home Page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,8 +1,20 @@
 import React, { Fragment } from 'react'
+import {CgMouse} from 'react-icons/cg'
+import "./Home.css"
 
 function Home() {
   return <Fragment>
-    
+    <div className="banner">
+
+        <p>Welcome to Grow app</p>
+        <h1>Find amazing product below</h1>
+
+        <a href="#container">
+            <button>
+                Scroll<CgMouse />
+            </button>
+        </a>
+    </div>
   </Fragment>
 }
 
