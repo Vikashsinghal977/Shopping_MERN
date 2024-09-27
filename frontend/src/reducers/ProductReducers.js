a8c18cb82b410e343446401be4f61e0d62de0080
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-27 20:30:20 +0530

    bump

--- frontend/src/reducers/ProductReducers.js
+++ frontend/src/reducers/ProductReducers.js
@@ -1,32 +1,34 @@
-import {ALL_PRODUCT_FAIL, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_REQUEST, CLEAR_ERRORS} from '../constants/ProductConstant';
+import { ALL_PRODUCT_FAIL, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_REQUEST, CLEAR_ERRORS } from '../constants/ProductConstant';
 
-export const ProductReducers = (state = { products: [] },action) => {
+let initialState = {
+    products: []
+}
 
-    
-        switch (action.type) {
-            case ALL_PRODUCT_REQUEST:
-                return{
-                    loading:true,
-                    products: [],
-                };
-            case ALL_PRODUCT_SUCCESS:
-                return{
-                    loading:false,
-                    products: action.payload.products,
-                    productsCount : action.payload.productsCount,
-                };
-            case ALL_PRODUCT_FAIL:
-                return{
-                    loading:false,
-                    error: action.payload,
-                }
-            case CLEAR_ERRORS:
-                return{
-                    ...state,
-                    error: null,
-                }
-            default:
-                return state;
-        }
+export const ProductReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading: true,
+                products: [],
+            };
+        case ALL_PRODUCT_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload.products,
+                productsCount: action.payload.productsCount,
+            };
+        case ALL_PRODUCT_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            }
+        default:
+            return state;
+    }
 
 }
