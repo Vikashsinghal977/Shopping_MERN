c7d1705eb6fb1c190de411a47cd6a89cfd53b98e
Vikash Singhal <vikashsinghal509@gmail.com>
2024-09-27 11:56:20 +0530

    Home Page almost complete

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from 'react'
 import {CgMouse} from 'react-icons/cg'
+import Product from "./Product.js"
 import "./Home.css"
+
+const product = {
+    name:"Baby wipes",
+    images:[{url:"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQVqYTj4yfEW5c8PCSRKT3jDSTIq0MQFR0V5Vl5-WRYhk5A4lDW-noBetbGheB1upAcyPx_5Lzm76Wv9oigD9sKMMA8CxBXv7Y60_B8sGcgd52BJLTuNg9xxCvHAvfK0_x2Xf12dIA&usqp=CAc"}],
+    price:"$3000",
+    _id:"Vikash"
+}
 
 function Home() {
   return <Fragment>
@@ -15,6 +23,20 @@ function Home() {
             </button>
         </a>
     </div>
+
+    <h2 className="homeHeading"> Featured Products </h2>
+
+    <div className='container' id='container'>
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+    </div>
+
   </Fragment>
 }
 
